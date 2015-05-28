89f83d0556b41e7916e4f0baf1eed842719cdc53
mikeyburns <mikb67@gmail.com>
2015-05-28 09:46:46 +0100

    Create BookNotFoundException.java

---
+++ BookNotFoundException.java
@@ -0,0 +1,17 @@
+package exercise.library;
+
+public class BookNotFoundException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BookNotFoundException(String message){
+	        super(message);
+	    }
+	
+	 
+	
+}
